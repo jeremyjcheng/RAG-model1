74a94924cef552ad32317b038c80fddbb61fce99
Jeremy Cheng <jeremy.jj.cheng@gmail.com>
2024-06-28 13:21:44 -0400

    Setting Up

--- model1.py
+++ model1.py
@@ -1,4 +1,8 @@
-from langchain_community.document_loaders import TextLoader
+from langchain.document_loaders.pdf import PyPDFDirectoryLoader
+def load_documents():
+    document_loader = PyPDFDirectoryLoader("data")
+    return document_loader.load()
 
-loader = TextLoader("life.pdf")
-loader.load()
+# Call the function to load documents
+documents = load_documents()
+print(documents)
